61e222f7247fd338f871c8bde84fdaf8cda928bc
Cory Frontin <cfrontin@mit.edu>
2023-04-11 21:27:24 -0400

    dummy mode activated

--- boxscore_cli/boxscore.py
+++ boxscore_cli/boxscore.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
 
+import os.path
+
+APP_DIR = os.path.split(__file__)[0]
+
+def print_dummy_linescore():
+
+    filename_linescore = os.path.join(APP_DIR, "linescore_dummy.txt")
+    with open(filename_linescore, 'r') as f:
+        linescore_str = f.read()
+
+    print()
+    print(linescore_str)
+    print()
+
+
+def print_dummy_boxscore():
+
+    filename_boxscore = os.path.join(APP_DIR, "boxscore_dummy.txt")
+    with open(filename_boxscore, 'r') as f:
+        boxscore_str = f.read()
+
+    print()
+    print(boxscore_str)
+    print()
+
+
 
 def main():
-    print("Hello, world!")
-    pass
 
+    print_dummy_linescore()
+
+    print_dummy_boxscore()
 
 if __name__ == "__main__":
     main()
