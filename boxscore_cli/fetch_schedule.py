e139e8d01a7ab63d7bf31c0717d0efe4ff07b9d4
Cory Frontin <cory.frontin@nrel.gov>
2024-05-09 09:51:22 -0600

    improved the fetchscores to include todays and yesterdays games based on the date

--- boxscore_cli/fetch_schedule.py
+++ boxscore_cli/fetch_schedule.py
@@ -1,4 +1,7 @@
 import os.path
+from datetime import datetime, timedelta
+
+import argparse
 from pprint import pprint
 
 import boxscore_cli.boxscore as boxscore
@@ -6,6 +9,21 @@ import boxscore_cli.tools_mlbapi as tools_mlbapi
 
 
 def main():
+    ### parse CLI arguments
+
+    parser = argparse.ArgumentParser(
+        prog="fetchscores",
+        description="cfrontin's CLI boxscore and linescore printer",
+        epilog="strike three!\a\n",
+    )
+    parser.add_argument("-t", "--today", action="store_true", default=False)
+    parser.add_argument("-y", "--yesterday", action="store_true", default=False)
+    parser.add_argument("-w", "--wide", action="store_true", default=False)
+    parser.add_argument("--debug", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+
     season = 2024
 
     mlbam_schedule_url = tools_mlbapi._MLB_SCHEDULE_FORMAT_STRING % (
@@ -20,8 +38,13 @@ def main():
     gamecount_by_date = {}
     games_by_date = {}
 
+    today = None
+    yesterday = None
+
     for date in sched_data["dates"]:
         date_of_games = date["date"]
+        is_today = datetime.strptime(date_of_games, '%Y-%m-%d').date() == datetime.today().date()
+        is_yesterday = datetime.strptime(date_of_games, '%Y-%m-%d').date() == (datetime.today() - timedelta(1)).date()
 
         total_games = 0
         scheduled_games = 0
@@ -31,7 +54,7 @@ def main():
         inprogress_games = 0
         postponed_games = 0
 
-        games_today = {
+        games_thisday = {
             "scheduled": [],
             "imminent": [],
             "cancelled": [],
@@ -52,25 +75,25 @@ def main():
             total_games += 1
             if codedGameState == "F":
                 completed_games += 1
-                games_today["completed"].append(gamePk)
+                games_thisday["completed"].append(gamePk)
             elif codedGameState == "O":
                 completed_games += 1
-                games_today["completed"].append(gamePk)
+                games_thisday["completed"].append(gamePk)
             elif codedGameState == "C":
                 cancelled_games += 1
-                games_today["cancelled"].append(gamePk)
+                games_thisday["cancelled"].append(gamePk)
             elif codedGameState == "D":
                 postponed_games += 1
-                games_today["postponed"].append(gamePk)
+                games_thisday["postponed"].append(gamePk)
             elif codedGameState == "P":
                 imminent_games += 1
-                games_today["imminent"].append(gamePk)
+                games_thisday["imminent"].append(gamePk)
             elif codedGameState == "S":
                 scheduled_games += 1
-                games_today["scheduled"].append(gamePk)
+                games_thisday["scheduled"].append(gamePk)
             elif codedGameState == "I":
                 inprogress_games += 1
-                games_today["inprogress"].append(gamePk)
+                games_thisday["inprogress"].append(gamePk)
             elif codedGameState is not None:
                 raise NotImplementedError(
                     f"codedGameState: {codedGameState} not yet handled."
@@ -86,7 +109,12 @@ def main():
             "postponed": postponed_games,
         }
 
-        games_by_date[date_of_games] = games_today
+        games_by_date[date_of_games] = games_thisday
+        
+        if is_today and completed_games > 0:
+            today = games_thisday
+        if is_yesterday and completed_games > 0:
+            yesterday = games_thisday
 
     gamecount_by_date = dict(sorted(gamecount_by_date.items()))
     days_with_completed = [
@@ -94,9 +122,28 @@ def main():
     ]
     last_day_completed = days_with_completed[-1]
 
-    for gamePk in games_by_date[last_day_completed]["completed"]:
-        print(f"gamePk: {gamePk}")
-        boxscore.print_linescore(gamePk, debug=False, wide=False)
+    if args.yesterday:
+        print("\nYESTERDAY'S GAMES:\n")
+        if yesterday is None:
+            print("No games completed yesterday.\n")
+        else:
+            for gamePk in yesterday["completed"]:
+                print(f"gamePk: {gamePk}")
+                boxscore.print_linescore(gamePk, debug=False, wide=args.wide)
+
+    if args.today:
+        print("\nTODAY'S GAMES:\n")
+        if today is None:
+            print("No games completed yet today.\n")
+        else:
+            for gamePk in today["completed"]:
+                print(f"gamePk: {gamePk}")
+                boxscore.print_linescore(gamePk, debug=False, wide=args.wide)
+
+    else:
+        for gamePk in games_by_date[last_day_completed]["completed"]:
+            print(f"gamePk: {gamePk}")
+            boxscore.print_linescore(gamePk, debug=False, wide=args.wide)
 
 
 if __name__ == "__main__":
