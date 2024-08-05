2db13ea774fb63f34b4b05a5c699cd9b29a654a5
Cory Frontin <cory.frontin@nrel.gov>
2024-08-05 15:59:06 -0600

    standings update

--- boxscore_cli/prototype_fetch_standings.py
+++ boxscore_cli/prototype_fetch_standings.py
@@ -56,206 +56,423 @@ division_map = {
     },
 }
 
-# create a list of the teams for a wildcard printout
-wc_list = []
-for lg_id, league_data in league_map.items():
-    lg_name = league_data["abbrev"]
+def run_standings():
 
-    mlbam_standings_url = tools_mlbapi._MLB_STANDINGS_FORMAT_STRING % league_data["id"]
-    standings_data = tools_mlbapi.download_json_url(
-        mlbam_standings_url,
+    # create a list of the teams for a wildcard printout
+    wc_list = []
+    for lg_id, league_data in league_map.items():
+        lg_name = league_data["abbrev"]
+
+        mlbam_standings_url = tools_mlbapi._MLB_STANDINGS_FORMAT_STRING % league_data["id"]
+        standings_data = tools_mlbapi.download_json_url(
+            mlbam_standings_url,
+        )
+
+        for record_div in standings_data["records"]:
+
+            id_div = record_div["division"]["id"]
+
+            for rank_tm, record_tm in enumerate(record_div["teamRecords"]):
+
+                name_team = record_tm["team"]["name"]
+                id_team = record_tm["team"]["id"]
+
+                wins_team = record_tm["wins"]
+                losses_team = record_tm["losses"]
+                wpct_team = wins_team / (wins_team + losses_team)
+                gb_team = record_tm["gamesBack"]
+                wcgb_team = record_tm["wildCardGamesBack"]
+                rs_team = record_tm["runsScored"]
+                ra_team = record_tm["runsAllowed"]
+                streak_team = record_tm["streak"]["streakCode"]
+
+                wc_list.append(
+                    {
+                        "name": name_team,
+                        "id": id_team,
+                        "wins": wins_team,
+                        "losses": losses_team,
+                        "wpct": wpct_team,
+                        "wcgb": wcgb_team,
+                        "streak": streak_team,
+                        "lg_id": lg_id,
+                        "div_id": id_div,
+                        "lg_name": lg_name,
+                        "div_name": division_map[id_div]["shortname"],
+                        "div_rank": rank_tm + 1,
+                        "rs_team": rs_team,
+                        "ra_team": ra_team,
+                        "rd_team": rs_team - ra_team,
+                    }
+                )
+
+
+    # create and sort standings dataframe
+    df_standings = pd.DataFrame(wc_list)
+    df_standings.sort_values("wpct", ascending=False, inplace=True)
+    df_standings.drop(
+        columns=[
+            "id",
+            "lg_id",
+            "div_id",
+        ],
+        inplace=True,
+    )
+    # df_standings.drop(columns=["id", "lg_id", "div_id", "lg_name",], inplace=True)
+
+    # rename to final header names
+    df_standings.rename(
+        columns={
+            "name": "TEAM",
+            "wins": "W",
+            "losses": "L",
+            "wpct": "PCT",
+            "wcgb": "WCGB",
+            "streak": "STK",
+            "rs_team": "RS",
+            "ra_team": "RA",
+            "rd_team": "RD",
+        },
+        inplace=True,
+    )
+    # streak lg_name    div_name  div_rank  rs_team  ra_team  rd_team
+
+    # title to length of field map
+    char_dict = {
+        "TEAM": 21,
+        "W": 3,
+        "L": 3,
+        "PCT": 5,
+        "WCGB": 5,
+        "STK": 3,
+        "RS": 4,
+        "RA": 4,
+        "RD": 4,
+        # "DIV": 10,
+        # "RK DIV": 6,
+    }
+
+    # create the lines for the standings display
+    sep_line = "•"
+    head_line = "•"
+    lg_lines = {
+        "AL": {
+            "head": "•",
+            "EAST": {"head": "•", "team_lines": ["•"] * 5},
+            "CENTRAL": {"head": "•", "team_lines": ["•"] * 5},
+            "WEST": {"head": "•", "team_lines": ["•"] * 5},
+        },
+        "NL": {
+            "head": "•",
+            "EAST": {"head": "•", "team_lines": ["•"] * 5},
+            "CENTRAL": {"head": "•", "team_lines": ["•"] * 5},
+            "WEST": {"head": "•", "team_lines": ["•"] * 5},
+        },
+    }
+
+    # make the pct pretty
+    df_standings.PCT = [f"{np.round(v,3):5.03f}" for v in df_standings.PCT]
+
+    # loop over the dictionary of columns
+    for k, v in char_dict.items():
+        head_line += " "
+        sep_line += "•"
+        for lg in ["AL", "NL"]:
+            lg_lines[lg]["head"] += " "  # if k == "TEAM" else "•"
+            for div in ["EAST", "CENTRAL", "WEST"]:
+                lg_lines[lg][div]["head"] += " "  # if k == "TEAM" else "•"
+                for idx, row in (
+                    df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                    .reset_index()
+                    .iterrows()
+                ):
+                    lg_lines[lg][div]["team_lines"][idx] += " "
+
+        head_line += f"{k:>{v}s}"
+        sep_line += "•" * v
+        for lg in ["AL", "NL"]:
+            if k == "TEAM":
+                lg_lines[lg][
+                    "head"
+                ] += f"{'AMERICAN LEAGUE' if lg == 'AL' else 'NATIONAL LEAGUE':<{v}s}"
+            else:
+                lg_lines[lg]["head"] += " " * v
+            for div in ["EAST", "CENTRAL", "WEST"]:
+                if k == "TEAM":
+                    lg_lines[lg][div]["head"] += f"{' '*2 + lg + ' ' + div:<{v}s}"
+                else:
+                    lg_lines[lg][div]["head"] += f"{k:>{v}s}"
+                for idx, row in (
+                    df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                    .reset_index()
+                    .iterrows()
+                ):
+                    lg_lines[lg][div]["team_lines"][idx] += f"{row[k]:>{v}}"
+
+        head_line += " •"
+        sep_line += "••"
+        for lg in ["AL", "NL"]:
+            lg_lines[lg]["head"] += " •" if k == "TEAM" else " •"
+            for div in ["EAST", "CENTRAL", "WEST"]:
+                lg_lines[lg][div]["head"] += " •" if k == "TEAM" else " •"
+                for idx, row in (
+                    df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                    .reset_index()
+                    .iterrows()
+                ):
+                    lg_lines[lg][div]["team_lines"][idx] += " •"
+
+    # make a standard standings printout
+
+    print()
+    print(sep_line)
+    print(lg_lines["AL"]["head"])
+    print(lg_lines["AL"]["EAST"]["head"])
+    print(sep_line)
+    for line in lg_lines["AL"]["EAST"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print(lg_lines["AL"]["CENTRAL"]["head"])
+    print(sep_line)
+    for line in lg_lines["AL"]["CENTRAL"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print(lg_lines["AL"]["WEST"]["head"])
+    print(sep_line)
+    for line in lg_lines["AL"]["WEST"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print()
+    print(sep_line)
+    print(lg_lines["NL"]["head"])
+    print(lg_lines["NL"]["EAST"]["head"])
+    print(sep_line)
+    for line in lg_lines["NL"]["EAST"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print(lg_lines["NL"]["CENTRAL"]["head"])
+    print(sep_line)
+    for line in lg_lines["NL"]["CENTRAL"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print(lg_lines["NL"]["WEST"]["head"])
+    print(sep_line)
+    for line in lg_lines["NL"]["WEST"]["team_lines"]:
+        print(line)
+    print(sep_line)
+    print()
+
+
+def run_wildcard():
+    # create a list of the teams for a wildcard printout
+    wc_list = []
+    for lg_id, league_data in league_map.items():
+        lg_name = league_data["abbrev"]
+
+        mlbam_standings_url = tools_mlbapi._MLB_STANDINGS_FORMAT_STRING % league_data["id"]
+        standings_data = tools_mlbapi.download_json_url(
+            mlbam_standings_url,
+        )
+
+        for record_div in standings_data["records"]:
+
+            id_div = record_div["division"]["id"]
+
+            for rank_tm, record_tm in enumerate(record_div["teamRecords"]):
+
+                name_team = record_tm["team"]["name"]
+                id_team = record_tm["team"]["id"]
+
+                wins_team = record_tm["wins"]
+                losses_team = record_tm["losses"]
+                wpct_team = wins_team / (wins_team + losses_team)
+                gb_team = record_tm["gamesBack"]
+                wcgb_team = record_tm["wildCardGamesBack"]
+                rs_team = record_tm["runsScored"]
+                ra_team = record_tm["runsAllowed"]
+                streak_team = record_tm["streak"]["streakCode"]
+
+                wc_list.append(
+                    {
+                        "name": name_team,
+                        "id": id_team,
+                        "wins": wins_team,
+                        "losses": losses_team,
+                        "wpct": wpct_team,
+                        "wcgb": wcgb_team,
+                        "streak": streak_team,
+                        "lg_id": lg_id,
+                        "div_id": id_div,
+                        "lg_name": lg_name,
+                        "div_name": division_map[id_div]["shortname"],
+                        "div_rank": rank_tm + 1,
+                        "rs_team": rs_team,
+                        "ra_team": ra_team,
+                        "rd_team": rs_team - ra_team,
+                    }
+                )
+
+
+    # create and sort standings dataframe
+    df_standings = pd.DataFrame(wc_list)
+    df_standings.sort_values("wpct", ascending=False, inplace=True)
+    df_standings.drop(
+        columns=[
+            "id",
+            "lg_id",
+            "div_id",
+        ],
+        inplace=True,
+    )
+    # df_standings.drop(columns=["id", "lg_id", "div_id", "lg_name",], inplace=True)
+
+    # rename to final header names
+    df_standings.rename(
+        columns={
+            "name": "TEAM",
+            "wins": "W",
+            "losses": "L",
+            "wpct": "PCT",
+            "wcgb": "WCGB",
+            "streak": "STRK",
+            "rs_team": "RS",
+            "ra_team": "RA",
+            "div_name": "DIV",
+            "div_rank": "RK",
+        },
+        inplace=True,
     )
 
-    for record_div in standings_data["records"]:
+    # title to length of field map
+    char_dict = {
+        "TEAM": 21,
+        "W": 3,
+        "L": 3,
+        "PCT": 5,
+        "WCGB": 5,
+        "STRK": 4,
+        "RS": 4,
+        "RA": 4,
+        "DIV": 3,
+    }
 
-        id_div = record_div["division"]["id"]
+    # create the lines for the standings display
+    sep_line = "•"
+    head_line = "•"
+    lg_lines = {
+        "AL": {
+            "head": "•",
+            "leader_lines": ["•"] * 3,
+            "wc_lines": ["•"] * 12,
+        },
+        "NL": {
+            "head": "•",
+            "leader_lines": ["•"] * 3,
+            "wc_lines": ["•"] * 12,
+        },
+    }
 
-        for rank_tm, record_tm in enumerate(record_div["teamRecords"]):
+    # make the pct pretty
+    df_standings.PCT = [f"{np.round(v,3):5.03f}" for v in df_standings.PCT]
+    df_standings.DIV = df_standings.DIV.str[3:].str[0].str.cat(df_standings.RK.astype(str))
 
-            name_team = record_tm["team"]["name"]
-            id_team = record_tm["team"]["id"]
-
-            wins_team = record_tm["wins"]
-            losses_team = record_tm["losses"]
-            wpct_team = wins_team / (wins_team + losses_team)
-            gb_team = record_tm["gamesBack"]
-            wcgb_team = record_tm["wildCardGamesBack"]
-            rs_team = record_tm["runsScored"]
-            ra_team = record_tm["runsAllowed"]
-            streak_team = record_tm["streak"]["streakCode"]
-
-            wc_list.append(
-                {
-                    "name": name_team,
-                    "id": id_team,
-                    "wins": wins_team,
-                    "losses": losses_team,
-                    "wpct": wpct_team,
-                    "wcgb": wcgb_team,
-                    "streak": streak_team,
-                    "lg_id": lg_id,
-                    "div_id": id_div,
-                    "lg_name": lg_name,
-                    "div_name": division_map[id_div]["shortname"],
-                    "div_rank": rank_tm + 1,
-                    "rs_team": rs_team,
-                    "ra_team": ra_team,
-                    "rd_team": rs_team - ra_team,
-                }
-            )
-
-
-# create and sort standings dataframe
-df_standings = pd.DataFrame(wc_list)
-df_standings.sort_values("wpct", ascending=False, inplace=True)
-df_standings.drop(
-    columns=[
-        "id",
-        "lg_id",
-        "div_id",
-    ],
-    inplace=True,
-)
-# df_standings.drop(columns=["id", "lg_id", "div_id", "lg_name",], inplace=True)
-
-# rename to final header names
-df_standings.rename(
-    columns={
-        "name": "TEAM",
-        "wins": "W",
-        "losses": "L",
-        "wpct": "PCT",
-        "wcgb": "WCGB",
-        "streak": "STK",
-        "rs_team": "RS",
-        "ra_team": "RA",
-        "rd_team": "RD",
-    },
-    inplace=True,
-)
-# streak lg_name    div_name  div_rank  rs_team  ra_team  rd_team
-
-# title to length of field map
-char_dict = {
-    "TEAM": 21,
-    "W": 3,
-    "L": 3,
-    "PCT": 5,
-    "WCGB": 5,
-    "STK": 3,
-    "RS": 4,
-    "RA": 4,
-    "RD": 4,
-    # "DIV": 10,
-    # "RK DIV": 6,
-}
-
-# create the lines for the standings display
-sep_line = "•"
-head_line = "•"
-lg_lines = {
-    "AL": {
-        "head": "•",
-        "EAST": {"head": "•", "team_lines": ["•"] * 5},
-        "CENTRAL": {"head": "•", "team_lines": ["•"] * 5},
-        "WEST": {"head": "•", "team_lines": ["•"] * 5},
-    },
-    "NL": {
-        "head": "•",
-        "EAST": {"head": "•", "team_lines": ["•"] * 5},
-        "CENTRAL": {"head": "•", "team_lines": ["•"] * 5},
-        "WEST": {"head": "•", "team_lines": ["•"] * 5},
-    },
-}
-
-# make the pct pretty
-df_standings.PCT = [f"{np.round(v,3):5.03f}" for v in df_standings.PCT]
-
-# loop over the dictionary of columns
-for k, v in char_dict.items():
-    head_line += " "
-    sep_line += "•"
-    for lg in ["AL", "NL"]:
-        lg_lines[lg]["head"] += " "  # if k == "TEAM" else "•"
-        for div in ["EAST", "CENTRAL", "WEST"]:
-            lg_lines[lg][div]["head"] += " "  # if k == "TEAM" else "•"
+    # loop over the dictionary of columns
+    for k, v in char_dict.items():
+        head_line += " "
+        sep_line += "•"
+        for lg in ["AL", "NL"]:
+            lg_lines[lg]["head"] += " "  # if k == "TEAM" else "•"
             for idx, row in (
-                df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] == 1)
+                ]
                 .reset_index()
                 .iterrows()
             ):
-                lg_lines[lg][div]["team_lines"][idx] += " "
+                lg_lines[lg]["leader_lines"][idx] += " "
+            for idx, row in (
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] != 1)
+                ]
+                .reset_index()
+                .iterrows()
+            ):
+                lg_lines[lg]["wc_lines"][idx] += " "
 
-    head_line += f"{k:>{v}s}"
-    sep_line += "•" * v
-    for lg in ["AL", "NL"]:
-        if k == "TEAM":
-            lg_lines[lg][
-                "head"
-            ] += f"{'AMERICAN LEAGUE' if lg == 'AL' else 'NATIONAL LEAGUE':<{v}s}"
-        else:
-            lg_lines[lg]["head"] += " " * v
-        for div in ["EAST", "CENTRAL", "WEST"]:
+        head_line += f"{k:>{v}s}"
+        sep_line += "•" * v
+        for lg in ["AL", "NL"]:
             if k == "TEAM":
-                lg_lines[lg][div]["head"] += f"{' '*2 + lg + ' ' + div:<{v}s}"
+                lg_lines[lg][
+                    "head"
+                ] += f"{'AMERICAN LEAGUE' if lg == 'AL' else 'NATIONAL LEAGUE':<{v}s}"
             else:
-                lg_lines[lg][div]["head"] += f"{k:>{v}s}"
+                lg_lines[lg]["head"] += f"{k:>{v}s}"  # " "*v
             for idx, row in (
-                df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] == 1)
+                ]
                 .reset_index()
                 .iterrows()
             ):
-                lg_lines[lg][div]["team_lines"][idx] += f"{row[k]:>{v}}"
-
-    head_line += " •"
-    sep_line += "••"
-    for lg in ["AL", "NL"]:
-        lg_lines[lg]["head"] += " •" if k == "TEAM" else " •"
-        for div in ["EAST", "CENTRAL", "WEST"]:
-            lg_lines[lg][div]["head"] += " •" if k == "TEAM" else " •"
+                lg_lines[lg]["leader_lines"][idx] += f"{row[k]:>{v}}"
             for idx, row in (
-                df_standings[df_standings["div_name"].str.upper() == f"{lg} {div}"]
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] != 1)
+                ]
                 .reset_index()
                 .iterrows()
             ):
-                lg_lines[lg][div]["team_lines"][idx] += " •"
+                lg_lines[lg]["wc_lines"][idx] += f"{row[k]:>{v}}"
 
-# make a standard standings printout
+        head_line += " •"
+        sep_line += "••"
+        for lg in ["AL", "NL"]:
+            lg_lines[lg]["head"] += " •" if k == "TEAM" else " •"
+            for idx, row in (
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] == 1)
+                ]
+                .reset_index()
+                .iterrows()
+            ):
+                lg_lines[lg]["leader_lines"][idx] += " •"
+            for idx, row in (
+                df_standings[
+                    (df_standings["lg_name"].str.upper() == lg) & (df_standings["RK"] != 1)
+                ]
+                .reset_index()
+                .iterrows()
+            ):
+                lg_lines[lg]["wc_lines"][idx] += " •"
 
-print()
-print(sep_line)
-print(lg_lines["AL"]["head"])
-print(lg_lines["AL"]["EAST"]["head"])
-print(sep_line)
-for line in lg_lines["AL"]["EAST"]["team_lines"]:
-    print(line)
-print(sep_line)
-print(lg_lines["AL"]["CENTRAL"]["head"])
-print(sep_line)
-for line in lg_lines["AL"]["CENTRAL"]["team_lines"]:
-    print(line)
-print(sep_line)
-print(lg_lines["AL"]["WEST"]["head"])
-print(sep_line)
-for line in lg_lines["AL"]["WEST"]["team_lines"]:
-    print(line)
-print(sep_line)
-print()
-print(sep_line)
-print(lg_lines["NL"]["head"])
-print(lg_lines["NL"]["EAST"]["head"])
-print(sep_line)
-for line in lg_lines["NL"]["EAST"]["team_lines"]:
-    print(line)
-print(sep_line)
-print(lg_lines["NL"]["CENTRAL"]["head"])
-print(sep_line)
-for line in lg_lines["NL"]["CENTRAL"]["team_lines"]:
-    print(line)
-print(sep_line)
-print(lg_lines["NL"]["WEST"]["head"])
-print(sep_line)
-for line in lg_lines["NL"]["WEST"]["team_lines"]:
-    print(line)
-print(sep_line)
-print()
+    # make a wildcard standings printout
+
+    print()
+    print(sep_line)
+    print(lg_lines["AL"]["head"])
+    print(sep_line)
+    for line in lg_lines["AL"]["leader_lines"]:
+        print(line)
+    print(sep_line)
+    for line in lg_lines["AL"]["wc_lines"]:
+        print(line)
+    print(sep_line)
+    print()
+    print(sep_line)
+    print(lg_lines["NL"]["head"])
+    print(sep_line)
+    for line in lg_lines["NL"]["leader_lines"]:
+        print(line)
+    print(sep_line)
+    for line in lg_lines["NL"]["wc_lines"]:
+        print(line)
+    print(sep_line)
+    print()
+
+if __name__ == "__main__":
+    print()
+    run_wildcard()
+    print()
+    run_standings()
+    print()
